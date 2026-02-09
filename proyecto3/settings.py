8c83c933b916b85737a6a4791a0c44ed54f8adc6
Alejandro Marcos Villarán <alejandro.marcos.villaran.alu@iesjulioverne.es>
2026-02-09 21:23:19 +0100

    segundo commit

--- proyecto3/settings.py
+++ proyecto3/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-57e&6s!==(wjipwamj7f_)3$f4=o%e#m5+w&0(x2@2)03fzz&@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 ROOT_URLCONF = "proyecto3.urls"
